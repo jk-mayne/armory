dbb3c92ca370c182e845b51609490108be196d5f
jk-mayne <pathfinder145@gmail.com>
2021-01-12 15:34:11 -0600

    attempting gowitness page again

---
+++ armory2/armory_main/included/webapps/gowitnessPage/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from armory2.armory_main.models import *
+from django.shortcuts import render, get_object_or_404
+from django.template.defaulttags import register
+from django.template import loader
+from django.views.decorators.csrf import csrf_exempt
+
+# from armory2.
+
+
+def index(request):
+
+   # basedomains = BaseDomain.objects.all().order_by('name')
+
+    return render(request, 'gowitnessPage/index.html')
